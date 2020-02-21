829f8ef4f38a09b401fcdda3ccd4ed4132f66f82
adamsteinPosting <60770588+adamsteinPosting@users.noreply.github.com>
2020-02-21 04:16:39 -0500

    Delete pupperLoader.ts

--- ts/pupperLoader.ts
+++
@@ -1,8 +0,0 @@
-const coincidencePupper = new CoincidencePupper();
-
-coincidencePupper.textNodesUnder(document.body);
-
-coincidencePupper.observer.observe(document.body, {
-  childList: true, // observe direct children
-  subtree: true
-});
